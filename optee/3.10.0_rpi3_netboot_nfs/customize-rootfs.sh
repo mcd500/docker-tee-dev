5b97b4c9cd014011a1cb8df07bea92eb9a6c48e0
Akira Tsukamoto <akira.tsukamoto@aist.go.jp>
2022-08-27 22:05:04 +0900

    Require using chroot for customizing rootfs

--- optee/3.10.0_rpi3_netboot_nfs/customize-rootfs.sh
+++ optee/3.10.0_rpi3_netboot_nfs/customize-rootfs.sh
@@ -1,11 +1,22 @@
 #!/bin/bash -xue
 
-# Copying files from host directory inside rootfs
-sudo cp /mnt/tee-supplicant.service /etc/systemd/system/
-sudo chmod 644 ${ROOTFS_EXT_DIR}/etc/systemd/system/tee-supplicant.service
+TARGET_DIR=/root/target
+ROOTFS_EXT_DIR=${TARGET_DIR}/arm64-20.04-rootfs
 
-# For optee library
-sudo ldconfig /usr/lib
+# Copying files from host directory inside rootfs
+cp /mnt/tee-supplicant.service /etc/systemd/system/
+chmod 644 ${ROOTFS_EXT_DIR}/etc/systemd/system/tee-supplicant.service
+
+# Update lib cache for installed optee library
+chroot ${ROOTFS_EXT_DIR} /bin/bash -c '
+ldconfig /usr/lib
+'
 
 # Booting tee-supplicant on power
-sudo systemctl enable tee-supplicant.service
+chroot ${ROOTFS_EXT_DIR} /bin/bash -c '
+systemctl enable tee-supplicant.service
+'
+
+# Update rootfs tar file
+cd ${TARGET_DIR}
+tar -cJf arm64-20.04-rootfs-optee.tar.xz --exclude="dev,proc,sys" arm64-20.04-rootfs && sudo mv arm64-20.04-rootfs-optee.tar.xz /
